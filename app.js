3f1bda5feaf66a2f50f58c31ad07ddafb6e7ced7
Simon Bencik <bencikdesign@gmail.com>
2020-01-18 19:05:09 +0100

    Added express middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const middleware = require('./middleware/express');
 const configure = require('./config/express');
 const routes = require('./config/routes');
 const app = express();
 
+app.use(middleware);
 app.use('/', routes);
 configure(app);
 
---
+++ middleware/express/index.js
@@ -0,0 +1,10 @@
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const express = require('express');
+
+module.exports = [
+    logger('dev'),
+    express.json(),
+    express.urlencoded({ extended: false }),
+    cookieParser(),
+]
