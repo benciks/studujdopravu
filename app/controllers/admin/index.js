d98bb7bbd902c1956d76f67258f5cf8c37a458e0
Simon Bencik <bencikdesign@gmail.com>
2020-02-01 19:50:16 +0100

    Fixed callback of nonexistent controller

--- app/controllers/admin/index.js
+++ app/controllers/admin/index.js
@@ -1,13 +1,7 @@
-const db = require('../../models/editorModel');
-
 exports.get = async (req, res) => {
   if (req.user) {
-    const result = await db.getArticle(3);
-    const content = result[0].content;
-
     res.render('admin/landing', {
       title: 'Admin',
-      content: content,
     });
   } else {
     res.redirect('/login')
