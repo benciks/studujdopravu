2758f94dd9bc416d571e338e43aee360447467e3
Simon Bencik <bencikdesign@gmail.com>
2020-02-01 20:59:42 +0100

    Fixed some little mistakes

--- app/controllers/admin/index.js
+++ app/controllers/admin/index.js
@@ -2,6 +2,7 @@ exports.get = async (req, res) => {
   if (req.user) {
     res.render('admin/landing', {
       title: 'Admin',
+      user: req.user.username
     });
   } else {
     res.redirect('/login')
